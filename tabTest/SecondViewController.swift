ff413263b70d7543a8ef0becda6a0b223a1580f3
Sergey Kozak <svkozak@icloud.com>
2017-12-12 17:30:03 -0500

    Added Orders tab and App icon

--- tabTest/SecondViewController.swift
+++ tabTest/SecondViewController.swift
@@ -8,7 +8,59 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+//    // Struct for importing Orders from API json
+//
+//    struct Order: Codable {
+//        var _id: String = ""
+//        var productName: String = ""
+//        var price: Double = 0
+//    }
+
+    // API Url
+    
+    let ordersUrl = URL(string: "https://serene-eyrie-60807.herokuapp.com/orders")
+    var orders = [CustomerOrdersViewController.Order]()
+    @IBOutlet weak var tableView: UITableView!
+    
+    // URL Session to get orders data from API
+    
+    func getOrders () {
+        URLSession.shared.dataTask(with: ordersUrl!) { (data, response, error) in
+            do {
+                self.orders = try JSONDecoder().decode([CustomerOrdersViewController.Order].self, from: data!)
+                
+                // To avoid warning from XCode tableView will be reloaded in main thread
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            } catch {
+                print(error)
+            }
+            }.resume()
+    }
+    
+    // Reload table data before view loads
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getOrders()
+    }
+    
+    // TableView implementation
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return orders.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "orderCell")
+        cell.textLabel?.text = orders[indexPath.row].product
+        cell.detailTextLabel?.text = "Customer ID: \(orders[indexPath.row].customerID ?? "unknown")"
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +71,9 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+ 
+    
 
 
 }
